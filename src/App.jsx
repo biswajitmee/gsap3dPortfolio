ce5974943d64affe66d773ddd9b978ac68d43e92
biswajitmee <biswajit.mee@gmail.com>
2024-03-22 17:55:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+ 
+import './App.css'
+import Navbarr from './pages/Navbarr'
+ 
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="menuIcon"> </div>
+ <Navbarr />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+ 
+
+function HomePage() {
+  return (
+    < >
+      
+<h1>Home page</h1>
+
+
+    </ >
+
+
+  )
+}
+
+export default HomePage
